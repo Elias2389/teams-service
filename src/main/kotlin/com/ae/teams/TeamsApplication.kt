e4b76eb3b0fd6b0dd155ac1b7dda37d8f1029b03
Andres <andres.rivas@mercadolibre.com>
2020-09-13 10:41:00 -0300

    Initial commit

---
+++ src/main/kotlin/com/ae/teams/TeamsApplication.kt
@@ -0,0 +1,11 @@
+package com.ae.teams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TeamsApplication>(*args)
+}
---
+++ src/test/kotlin/com/ae/teams/TeamsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ae.teams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
